fc4f1c37d0261ddc20ccb13fd46b7494badb22db
sim123 <simonadragomir2001@gmail.com>
2023-11-08 20:45:21 -0500

    added MotionCollision.cs

---
+++ MotionColision.cs
@@ -0,0 +1,72 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace web_api.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class CollisionSensorController : ControllerBase
+  {
+    private readonly string[] PostalCodes = { "H2L 4T9", "H2J 4B4", "H3H 1Y3", "H1Z 3A7" };
+    private readonly string Type = {"motion", "colision"};
+
+    private readonly ILogger<CollisionSensorController> _logger;
+
+    public CollisionSensorController(ILogger<CollisionSensorController> logger)
+    {
+      _logger = logger;
+    }
+
+    [HttpGet(Name = "GetMotionCollision")]
+    [ProducesResponseType(typeof(IEnumerable<MotionCollision>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+    [Produces("application/json")]
+    public ActionResult<Object> Get()
+    {
+      string thePostalCode = RandomPostalCode();
+      string theType = RandomDetection();
+      bool theValue = RandomValue();
+      var theDetection = {'type':theType,'value':theValue};
+      var theDate = DateOnly.FromDateTime(DateTime.Now);
+      string thePicture = "picture"
+      
+      return new MotionCollision
+      {
+        Date = theDate,
+        PostalCode = thePostalCode,
+        Detection = theDetection,
+        Picture = thePicture
+      };
+
+    }
+    
+    private bool RandomValue()
+    {
+		var random = new Random();
+		return random.Next(2) == 1;
+	}
+	
+	private string RandomDetection()
+    {
+		var random = new Random();
+		return Type[random.Next(2)];
+	}
+	
+	private string RandomPostalCode()
+	{
+		var random = new Random();
+		int ran = andom.Next(5);
+		for (int i = 0; i < 5; i++)
+		{
+			if (i == ran)
+			{
+				return PostalCodes[ran];
+			}
+		}
+	}
+	
+  }
+}
