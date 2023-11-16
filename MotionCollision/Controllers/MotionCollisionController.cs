a1438ce9ff180d0b81801be94539f098817c4635
PepeLaugh <charles-alexandre.bouchard@dawsoncollege.qc.ca>
2023-11-16 12:23:11 -0500

    fix camera logic (hopefully)

--- MotionCollision/Controllers/MotionCollisionController.cs
+++ MotionCollision/Controllers/MotionCollisionController.cs
@@ -1,14 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using MMALSharp;
-using MMALSharp.Common.Utility;
 using MMALSharp.Components;
 using MMALSharp.Handlers;
+using MMALSharp.Native;
+using MMALSharp.Ports;
+using MMALSharp.Ports.Outputs;
+using MMALSharp.Common;
 
 namespace web_api.Controllers
 {
@@ -17,16 +19,14 @@ namespace web_api.Controllers
     public class CollisionSensorController : ControllerBase
     {
         private readonly string[] PostalCodes = { "H2L 4T9", "H2J 4B4", "H3H 1Y3", "H1Z 3A7", "H2H 1S9" };
-        private readonly string[] Types = { "motion", "collision" };
+        private readonly string[] Type = { "motion", "collision" };
+        private MMALCamera camera = MMALCamera.Instance;
 
         private readonly ILogger<CollisionSensorController> _logger;
 
         public CollisionSensorController(ILogger<CollisionSensorController> logger)
         {
             _logger = logger;
-
-            // Initialize MMALSharp
-            MMALCamera.ConfigureCameraSettings();
         }
 
         [HttpGet(Name = "GetMotionCollision")]
@@ -35,45 +35,22 @@ namespace web_api.Controllers
         [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
         [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
         [Produces("application/json")]
-        public ActionResult<IEnumerable<MotionCollision>> Get()
+        public ActionResult<Object> Get()
         {
             string thePostalCode = RandomPostalCode();
             string theType = RandomDetection();
             bool theValue = RandomValue();
-            var theDetection = new Detection { Type = theType, Value = theValue };
+            var theDetection = new Detection { type = theType, value = theValue };
             var theDate = DateOnly.FromDateTime(DateTime.Now);
-            string pictureFileName = CapturePictureOnCollision(theType);
+            string thePicture = CapturePicture(); // Call the method to capture a picture
 
             return Enumerable.Range(1, 5).Select(index => new MotionCollision
             {
                 Date = theDate,
                 PostalCode = thePostalCode,
-                TheDetection = theDetection,
-                Picture = pictureFileName
+                theDetection = theDetection,
+                Picture = thePicture
             }).ToArray();
-        }
-
-        private string CapturePictureOnCollision(string detectionType)
-        {
-            if (detectionType.ToLower() == "collision")
-            {
-                // Capture a picture when a collision is detected
-                string pictureFileName = $"collision_picture_{DateTime.Now:yyyyMMddHHmmss}.jpg";
-                CapturePicture(pictureFileName);
-                return pictureFileName;
-            }
-
-            return "no_picture.jpg"; // Return a placeholder or empty string if no collision
-        }
-
-        private void CapturePicture(string fileName)
-        {
-            // Capture a picture using the Raspberry Pi camera (MMALSharp)
-            using (var captureHandler = new ImageStreamCaptureHandler(fileName, MMALEncoding.JPEG))
-            {
-                MMALCamera.Instance.TakePicture(captureHandler, MMALEventType.MMAL_EVENT_TIMEOUT);
-                captureHandler.Wait();
-            }
         }
 
         private bool RandomValue()
@@ -85,14 +62,34 @@ namespace web_api.Controllers
         private string RandomDetection()
         {
             var random = new Random();
-            return Types[random.Next(2)];
+            return Type[random.Next(2)];
         }
 
         private string RandomPostalCode()
         {
             var random = new Random();
             int ran = random.Next(5);
-            return PostalCodes[ran];
+            for (int i = 0; i < 5; i++)
+            {
+                if (i == ran)
+                {
+                    return PostalCodes[ran];
+                }
+            }
+            return "";
+        }
+
+        private string CapturePicture()
+        {
+                // Create a new image capture handler
+                using (var imgCaptureHandler = new ImageStreamCaptureHandler("/path/to/save/image.jpg", "jpg"))
+                {
+                    // Take a picture
+                    camera.TakePicture(imgCaptureHandler, MMALEncoding.JPEG, MMALEncoding.I420);
+                }
+
+                // Return the path to the captured image
+                return "../image.jpg";
         }
     }
 }
